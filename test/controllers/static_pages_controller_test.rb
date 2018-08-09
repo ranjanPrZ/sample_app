fc70c6ee7a68f2c00c91fa778a7b0a911ac0fb19
ranjanPrZ <ranjantabby@gmail.com>
2018-08-08 19:02:01 -0700

    done basic setup, created toy app, created advance setup,added helper,created controllers and views for user and static_pages created models for users

---
+++ test/controllers/static_pages_controller_test.rb
@@ -0,0 +1,29 @@
+require 'test_helper'
+
+class StaticPagesControllerTest < ActionController::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+
+  def setup
+  	@base_title = "Ruby on rails Tutorial Sample App"
+  end
+  
+  test "should get home" do
+    get static_pages_home_url
+  	assert_response :success
+  	assert_select "title", "Ruby on Rails Tutorial Sample App"
+  end
+
+  test "should get help" do
+  	get static_pages_help_url
+  	assert_response :success
+  	assert_select "title", "Ruby on Rails Tutorial Sample App"
+  end
+
+  test "should get about" do
+  	get static_pages_about_url
+  	assert_response :success
+  	assert_select "title", "Ruby on Rails Tutorial Sample App"
+  end
+end
